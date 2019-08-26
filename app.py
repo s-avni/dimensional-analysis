2d3f0e47089cfae98aca88717eb4000dba0a221a
shiri <avni.shiri@gmail.com>
2019-08-26 18:22:35 +0300

    added footer; fixed bugs related to variable exponents

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ REMOVE_A_VAR = "REMOVE A VARIABLE" # e.g. DUMMY = [mass], m=[mass], T=[time], f=
 
 
 def get_pint_vars_from_form(form):
+    """
+    Ensure the variables in the form are correct, and that there is at least 1 variable,
+    and that entries are properly filled out
+
+    :param form: a form with entries {variable name, variable dimension}
+    :return: a dictionary with key = variable name, value = variable dimension
+    """
     vars_dict = dict()
     for entry in form.vars.entries:
         var_name = "" if entry.var_name.data is None else entry.var_name.data
@@ -27,13 +34,19 @@ def get_pint_vars_from_form(form):
             return ERROR + "you have a partly filled entry"
         if var_name.strip() == "" and var_dim.strip() == "":
             continue  # no input
-        if "/" in var_name or " " in var_name.strip():  # due to regex later, when formatting result
-            return ERROR + "there can be no spaces or slashes (/) in a variable name"
+        forbidden_chars = set('0123456789^/* ') # due to regex later, when formatting result
+        if any((c in var_name.strip()) for c in forbidden_chars):
+            return ERROR + "there can be none of the following characters in a variable name: '0123456789^/* '"
         vars_dict[var_name.strip()] = var_dim.strip()
     return vars_dict
 
 
 def calc_dimensionless_form(form):
+    """
+
+    :param form: a form with entries {variable name, variable dimension}
+    :return: a string of the proper combination, or an error message if a problem occurs
+    """
     result = get_pint_vars_from_form(form)
     if isinstance(result, str):  # an ERROR string
         return result
@@ -43,19 +56,22 @@ def calc_dimensionless_form(form):
 
 
 def format_dimension_result(res):
+    """
+
+    :param res: the dimensionless string combination provided by pint, which most likely includes DUMMY, and so needs to be reformated. heavy lifting happens here
+    :return: reformated string, with dimension "desired dimension", or an error message
+    """
     # change dimensionless result to the combination we want
     # e.g. if want to express D using V and X, we get D*V/X = [], so want X/V
     # that is, we flip the order of the substrings, split by "/
     # e.g. if T/DV =[], then D = T/V #no flipping!
-    print("\n ********* \n")
-    print(res)
-    # Note!
-    # e.g. DUMMY = [mass], m=[mass], T=[time], f=1/[time] -->
-    # could return T*f because that is already a solution.
-    # so, we need to ensure DUMMY is in solution! remove the other vars...
-    if DUMMY not in res: #e.g. DUMMY = [mass], T=[time], f=1/[time]
+    if DUMMY not in res: #Note! Ensure dummy in solution: e.g. DUMMY = [mass], T=[time], f=1/[time]
         return REMOVE_A_VAR
-        # return ERROR + ": there is no suitable combination to give your desired dimension."
+    dummy_power = get_power_of_dummy_var(res)
+    if dummy_power != "":
+        res = res.replace(DUMMY+"^"+dummy_power, DUMMY) #easier to work with DUMMY only, use power later
+    print("dummy power")
+    print(dummy_power)
     split_result = res.split("/")
     assert len(split_result) == 2
     fixed_parts = [None, None]  # what goes first, what goes second, split by "/"
@@ -72,12 +88,54 @@ def format_dimension_result(res):
             fixed_parts = [other_part, this_part]
 
     logging.debug(fixed_parts)
+    # avoid situations like: (T) / V or T / (V)
+    for i in [0, 1]:
+        if "*" not in fixed_parts[i]:
+            print("yes")
+            print(fixed_parts[i])
+            fixed_parts[i] = fixed_parts[i].replace("(", "")
+            fixed_parts[i] = fixed_parts[i].replace(")", "")
+        #avoid random whitespace
+        fixed_parts[i] = fixed_parts[i].strip()
+
+    # DUMMY^2 / V --> [DUMMY] = V^(1/2)
+    # V / DUMMY^2 --> [DUMMY] = V^(1/2)
+    # X * DUMMY^2 / V --> [DUMMY] = (V/X)^(1/2)
     if fixed_parts[1] == "1":  # e.g. no need to write V / 1
-        return fixed_parts[0]
-    return fixed_parts[0] + "/" + fixed_parts[1]
+        if dummy_power == "":
+            return fixed_parts[0]
+        else:
+            if "*" in fixed_parts[0]: #e.g. (T * V)^(1/5)
+                return "(" + fixed_parts[0] + ")^(1/" + dummy_power + ")"
+            else:
+                return fixed_parts[0] + "^(1/" + dummy_power + ")" #e.g T^(1/6)
+    if dummy_power == "":
+        return fixed_parts[0] + " / " + fixed_parts[1]
+    else:
+        return "("+fixed_parts[0] + " / " + fixed_parts[1]+")^(1/" + dummy_power + ")"
 
 
-def get_correct_combination_for(vars_dict):
+def get_power_of_dummy_var(res: str) -> str:
+    """
+
+    :param res: the combination string formatted by Pint for the dimension problem, e.g. DUMMY^5 / V
+    :return: the power of DUMMY, e.g. 5 above, or "" if no power
+    """
+    matcher = re.search(DUMMY + "\^(\d*)", res)
+    if matcher is None: #dummy is not to the power of anything
+        return ""
+    return matcher.group(1)
+
+
+def get_combination_for_desired_dimension(vars_dict):
+    """
+    This function helps to ensure that the dimensionless combination that Pint returns includes
+    the DUMMY variable. Otherwise, we can get a dimensionless combination that ignores DUMMY.
+    e.g. DUMMY [M], t = [T], f = 1/[T] --> DUMMY would be ignored
+
+    :param vars_dict: a dict of variable names and dimensions, with a DUMMY variable for desired dimension
+    :return: string combination or an error message
+    """
     while len(vars_dict) != 1: #i.e. DUMMY is not the only variable
         result = get_combination_for(vars_dict)
         if result.startswith(ERROR):
@@ -87,7 +145,7 @@ def get_correct_combination_for(vars_dict):
             return dimensionless_string
 
         # we have at least 1 dependent var that is not dummy
-        # take the first var before the first * or / (whichever comes first); we know it's not DUMMy
+        # take the first var before the first * or / (whichever comes first); we know it's not DUMMY
         unneeded_dimension_name = result.split("*")[0]
         unneeded_dimension_name = unneeded_dimension_name.split("/")[0]
         vars_dict.pop(unneeded_dimension_name.strip())
@@ -95,6 +153,11 @@ def get_correct_combination_for(vars_dict):
 
 
 def calc_dimension_form(form):
+    """
+
+    :param form: a form with entries {variable name, variable dimension}, and a desired dimension
+    :return: a string of variable combinations which gives desired dimension, or an error message
+    """
     other_vars = get_pint_vars_from_form(form)
     if isinstance(other_vars, str):  # an ERROR string
         return other_vars
@@ -102,10 +165,15 @@ def calc_dimension_form(form):
     if desired_dim.strip() == "":
         return ERROR + ": the desired dimension must be provided."
     other_vars[DUMMY] = desired_dim.strip()  # we'll remove dummy later, first want a dimensionless result
-    return get_correct_combination_for(other_vars)
+    return get_combination_for_desired_dimension(other_vars)
 
 
 def get_combination_for(vars_dict):
+    """
+
+    :param vars_dict: a dictionary of variable names and dimensions
+    :return: a prettily formatted _dimensionless_ combination of the variables, or an error message
+    """
     logging.debug('list of vars, in dict format:')
     logging.debug(vars_dict)
     if len(vars_dict) == 0:
@@ -115,9 +183,9 @@ def get_combination_for(vars_dict):
         return ERROR + "it is not possible to find a dimensionless combination for your input"
     print("\n*************\n")
     print(result)
-    print(formatter(result[0].items(), single_denominator=True, power_fmt='({}^{})'))
+    print(formatter(result[0].items(), single_denominator=True, power_fmt='{}^{}'))
     # raise ValueError
-    pretty_result = formatter(result[0].items(), single_denominator=True, power_fmt='({}^{})')
+    pretty_result = formatter(result[0].items(), single_denominator=True, power_fmt='{}^{}')
     return pretty_result
 
 
@@ -154,3 +222,4 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+
--- pint_wrapper/test_app.py
+++ pint_wrapper/test_app.py
@@ -1,21 +1,48 @@
 from unittest import TestCase
 import unittest
 
-from app import format_dimension_result, DUMMY
+from app import format_dimension_result, DUMMY, get_power_of_dummy_var
 
 
 class TestFormat_dimension_result(TestCase):
     def test_format_dimension_result(self):
+        #dummy top
         res = DUMMY + " / L"
         self.assertEqual(format_dimension_result(res).strip(), "L")
-        res = DUMMY + " * T / L"
-        self.assertEqual(format_dimension_result(res).strip(), "L/T")
-        res = "T * " + DUMMY + " / L"
-        self.assertEqual(format_dimension_result(res).strip(), "L/T")
-        res = " T / V * " + DUMMY
+        res = "( " + DUMMY + " * T ) / L"
+        self.assertEqual(format_dimension_result(res).strip(), "L / T")
+        res = "( T * " + DUMMY + " ) / L"
+        self.assertEqual(format_dimension_result(res).strip(), "L / T")
+
+        #dummy bottom
+        res = " T / ( V * " + DUMMY + ")"
+        self.assertEqual(format_dimension_result(res).strip(), "T / V")
+        res = " T / ( " + DUMMY + " * V )"
         self.assertEqual(format_dimension_result(res).strip(), "T / V")
         res = " T / " + DUMMY
         self.assertEqual(format_dimension_result(res).strip(), "T")
+
+        #dummy power on bottom
+        res = " T * V / " + DUMMY + "^5"
+        self.assertEqual(format_dimension_result(res).strip(), "(T * V)^(1/5)")
+        res = " T / ( V * " + DUMMY + "^5 )"
+        self.assertEqual(format_dimension_result(res).strip(), "(T / V)^(1/5)")
+        res = " T / " + DUMMY + "^5"
+        self.assertEqual(format_dimension_result(res).strip(), "T^(1/5)")
+        res = DUMMY + "^5 / T "
+        self.assertEqual(format_dimension_result(res).strip(), "T^(1/5)")
+        res = DUMMY + "^5 * V / T "
+        self.assertEqual(format_dimension_result(res).strip(), "(T / V)^(1/5)")
+        res = "V * " + DUMMY + "^5 / T "
+        self.assertEqual(format_dimension_result(res).strip(), "(T / V)^(1/5)")
+
+    def test_get_power_of_dummy_var(self):
+        res = DUMMY + "^10"
+        self.assertEqual(get_power_of_dummy_var(res), "10")
+        res = DUMMY
+        self.assertEqual(get_power_of_dummy_var(res), "")
+        res = "V * " + DUMMY + "^5"
+        self.assertEqual(get_power_of_dummy_var(res), "5")
 
 if __name__ == "__main__":
     unittest.main()
